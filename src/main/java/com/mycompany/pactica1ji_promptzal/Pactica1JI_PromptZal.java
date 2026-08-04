154a99ebddbc16b4b3611be7d7d3696884339a7f
Jesua Ixcolin <jesuaixcolin3a@gmail.com>
2026-08-03 22:45:52 -0600

    Estructura Inicial de la Practica1 JI

---
+++ src/main/java/com/mycompany/pactica1ji_promptzal/Pactica1JI_PromptZal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pactica1ji_promptzal;
+
+/**
+ *
+ * @author jesua
+ */
+public class Pactica1JI_PromptZal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/promptZal/reporteHTML.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package promptZal;
+
+/**
+ *
+ * @author jesua
+ */
+public class reporteHTML {
+    
+}
